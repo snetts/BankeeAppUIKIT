a7daf19519f034fb03b319a9f8f13817a1117f2a
rohansurve <rohan.surve5@gmail.com>
2020-10-04 14:10:20 +0530

    my account screen added

---
+++ app/src/main/java/com/ronnystudio/bankeeappuikit/MyAccountScreen.java
@@ -0,0 +1,14 @@
+package com.ronnystudio.bankeeappuikit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyAccountScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account_screen);
+    }
+}
