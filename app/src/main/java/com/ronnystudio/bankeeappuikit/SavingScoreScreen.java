0c10c3093da431150c962e642e5a168b5711f1ee
rohansurve <rohan.surve5@gmail.com>
2020-10-13 13:44:32 +0530

    score screen added

---
+++ app/src/main/java/com/ronnystudio/bankeeappuikit/SavingScoreScreen.java
@@ -0,0 +1,14 @@
+package com.ronnystudio.bankeeappuikit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavingScoreScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saving_score_screen);
+    }
+}
